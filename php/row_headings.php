d1aec0d1b3a3b68a99c110361d01efc81c03604c
Kevin Lin <kl738@cornell.edu>
2017-06-15 18:13:37 -0400

    row headings

--- php/row_headings.php
+++ php/row_headings.php
@@ -3,27 +3,27 @@
 		<div clas="row">
 			<div class="col-sm-4">
             <div class="post">
-	            <h3><a href="text.html">Research</a></h3>
+	            <h3><a href="research.php">Research</a></h3>
 	            <div class="image"><a href="research.php"><img src="img/blog1.jpg" alt="" class="img-responsive"></a></div>
 	            <p class="post__intro">Our research projects and interests</p>
-	            <p class="read-more"><a href="text.html" class="btn btn-ghost">Continue reading     </a></p>
-            </div>
+<!-- 	            <p class="read-more"><a href="text.html" class="btn btn-ghost">Continue reading     </a></p>
+ -->            </div>
           </div>
           <div class="col-sm-4">
             <div class="post">
-              <div class="image"><a href="text.html"><img src="img/blog2.jpg" alt="" class="img-responsive"></a></div>
-              <h3><a href="text.html">Tempor sit amet</a></h3>
-              <p class="post__intro"> Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.</p>
-              <p class="read-more"><a href="text.html" class="btn btn-ghost">Continue reading     </a></p>
-            </div>
+            	<h3><a href="text.html">Publications</a></h3>
+              	<div class="image"><a href="publications.php"><img src="img/blog2.jpg" alt="" class="img-responsive"></a></div>
+              	<p class="post__intro">Papers and Talks</p>
+<!--              	 <p class="read-more"><a href="text.html" class="btn btn-ghost">Continue reading     </a></p>
+ -->            </div>
           </div>
           <div class="col-sm-4">
             <div class="post">
-              <div class="image"><a href="text.html"><img src="img/blog3.jpg" alt="" class="img-responsive"></a></div>
-              <h3><a href="text.html">Vestibulum erat wisi</a></h3>
-              <p class="post__intro">ellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-              <p class="read-more"><a href="text.html" class="btn btn-ghost">Continue reading     </a></p>
-            </div>
+            	<h3><a href="text.html">People</a></h3>
+              	<div class="image"><a href="people.php"><img src="img/blog3.jpg" alt="" class="img-responsive"></a></div>
+              	<p class="post__intro">Lab Members</p>
+<!--               	<p class="read-more"><a href="text.html" class="btn btn-ghost">Continue reading     </a></p>
+ -->            </div>
           </div>
 		</div>
 	</div>

2acb44d00ccca7c4397696286fa4935984b02edf
Kevin Lin <kl738@cornell.edu>
2017-06-16 13:46:32 -0400

    event

--- index.php
+++ index.php
@@ -39,8 +39,5 @@
     ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.cookie.js"> </script>
-    <script src="js/lightbox.min.js"></script>
-    <script src="js/front.js"></script>
 	</body>
 </html>
--- php/news.php
+++ php/news.php
@@ -1,41 +1,50 @@
 <section class="background-white section--no-padding">
 	<div class="container">
-		<h2>Recent Events</h2>
+		<h2>Recent News</h2>
 	</div>
 </section>
 <section class="background-white">
 	<div class="container">
-		<div id="carousel-home" data-ride="carousel" class="carousel slide carousel-fullscreen carousel-fade">
+		<div id="carousel-home" data-ride="carousel" class="carousel slide carousel-fullscreen carousel-fade" data-interval="3000">
 	      	<!-- Indicators-->
-	      	<ol class="carousel-indicators">
+	      	<!-- <ol class="carousel-indicators">
 	        	<li data-target="#carousel-home" data-slide-to="0" class="active"></li>
 	        	<li data-target="#carousel-home" data-slide-to="1"></li>
 	        	<li data-target="#carousel-home" data-slide-to="2"></li>
-	      	</ol>
+	      	</ol> -->
       		<!-- Wrapper for slides-->
 		    <div role="listbox" class="carousel-inner">
 		        <div style="background-image: url('img/carousel1.jpg');" class="item active">
 		          	<div class="overlay"></div>
 		          		<div class="carousel-caption">
-		            		<h1 class="super-heading">Event</h1>
+		            		<h1 class="super-heading">Event 1 </h1>
 		            		<p class="super-paragraph">Caption</p>
 		          		</div>
 		        </div>
 		        <div style="background-image: url('img/carousel2.jpg');" class="item">
 		          	<div class="overlay"></div>
 		          		<div class="carousel-caption">
-		            		<h1 class="super-heading">Event</h1>
+		            		<h1 class="super-heading">Event 2 </h1>
 		            		<p class="super-paragraph">Caption</p>
 		          	</div>
 		        </div>
 		        <div style="background-image: url('img/carousel3.jpg');" class="item">
 		          	<div class="overlay"></div>
 		          		<div class="carousel-caption">
-		           			<h1 class="super-heading">Event Caption</h1>
+		           			<h1 class="super-heading">Event 3</h1>
 		            		<p>Caption</p>
 		          	</div>
 		        </div>
 		    </div>
+		    <!--Left Right Arrows-->
+		    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			    <span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			    <span class="sr-only">Next</span>
+			</a>
     	</div>
 	</div>
 </section>

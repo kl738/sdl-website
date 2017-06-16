33b3e9276c8355fdc0f5ff8b9f29646509d74928
Kevin Lin <kl738@cornell.edu>
2017-06-16 15:22:21 -0400

    names

--- php/news.php
+++ php/news.php
@@ -37,14 +37,14 @@
 		        </div>
 		    </div>
 		    <!--Left Right Arrows-->
-		    <a class="left carousel-control" href="#carousel-home" data-slide="prev">
+		    <!-- <a class="left carousel-control" href="#carousel-home" data-slide="prev">
 			    <span class="glyphicon glyphicon-chevron-left"></span>
 			    <span class="sr-only">Previous</span>
 			</a>
 			<a class="right carousel-control" href="#carousel-home" data-slide="next">
 			    <span class="glyphicon glyphicon-chevron-right"></span>
 			    <span class="sr-only">Next</span>
-			</a>
+			</a> -->
     	</div>
 	</div>
 </section>
--- research.php
+++ research.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Blog Post</title>
+        <title>Research</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="all,follow">
@@ -34,14 +34,27 @@
         ?>
         <section class="section--no-padding">
             <div class="container">
-                <p class="lead">We study the interplay between network topology and the dynamics of social interaction, using computational models, data from on-line networks, and laboratory experiments with human participants.
-                </p>
+                <div class="row">
+                    <div class="col-sm-8">
+                        <p class="lead">We study the interplay between network topology and the dynamics of social interaction, using computational models, data from on-line networks, and laboratory experiments with human participants.
+                        </p>
 
-                <p class="lead">We are supported by grants from the National Science Foundation and the MINERVA Initiative.</p>
+                        <p class="lead">We are supported by grants from the National Science Foundation and the MINERVA Initiative.</p>
 
-                <p class="lead">The SDL Director is Michael Macy, Goldwin Smith Professor of Arts and Sciences at Cornell University, with a joint appointment in the Department of Sociology and the Department of Information Science.</p>
+                        <p class="lead">The SDL Director is Michael Macy, Goldwin Smith Professor of Arts and Sciences at Cornell University, with a joint appointment in the Department of Sociology and the Department of Information Science.</p>
 
-                <p class="lead">The Assistant Director is Chris Cameron, a PhD candidate at Cornell.</p>
+                        <p class="lead">The Assistant Director is Chris Cameron, a PhD candidate at Cornell.</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="img/nsf.png" alt="" class="img-responsive">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section--padding-small">
+            <div class="container">
+                <h2>Current Projects</h2>
+                <h2>Past Projects</h2>
             </div>
         </section>
 

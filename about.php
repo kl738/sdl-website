426f14ffd44c8e6ccaecbda6d16204b55a61377f
Kevin Lin <kl738@cornell.edu>
2017-06-19 14:22:22 -0400

    removed unnecessary files

--- about.php
+++
@@ -1,8 +0,0 @@
-<section class="background-white">
-  	<div class="container">
-	    
-	    <p class="lead">We study the interplay between network topology and the dynamics of social interaction, using computational models, data from on-line networks, and laboratory experiments with human participants.</p>
-	    <p> <a href="research.php" class="btn btn-ghost">More about our research</a></p>
-	    <h2>About Us</h2>
-  	</div>
-</section>

f00e689c8da3d05ce9189b5fd5d334ea83d792e3
Kevin Lin <kl738@cornell.edu>
2017-06-19 14:02:43 -0400

    events page

--- php/nav.php
+++ php/nav.php
@@ -12,6 +12,7 @@
           <li <?php if ($activePage =="research") {?>class="active" <?php } ?>><a href="research.php">Research</a></li>
           <li <?php if ($activePage =="publications") {?>class="active" <?php } ?>><a href="publications.php">Publications</a></li>
           <li <?php if ($activePage =="members") {?>class="active" <?php } ?>><a href="members.php">Lab Members</a></li>
+          <li <?php if ($activePage =="events") {?>class="active" <?php } ?>><a href="events.php">Events</a></li>
           <li <?php if ($activePage =="contact") {?>class="active" <?php } ?>><a href="contact.php">Contact</a></li>
         </ul>
       </div>

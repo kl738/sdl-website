62678e95240673ee17e6972e4bcec425322d56e8
Kevin Lin <kl738@cornell.edu>
2017-08-14 14:26:09 -0400

    updated config file and imported sql from phpmyadmin to server

--- php/config.php
+++ php/config.php
@@ -2,11 +2,17 @@
 // ** MySQL connection settings ** //
 
 //Keep this as localhost for the course server and for local server with MAMP
-define('DB_HOST', "localhost");
+//define('DB_HOST', "localhost");
+define('DB_HOST', "us-cdbr-iron-east-05.cleardb.net");
 
-define('DB_USER', "klin");    
- 
-define('DB_PASSWORD', "kevin123"); 
+// define('DB_USER', "klin");    
+define('DB_USER', "bda716fd54f2bf"); 
 
-define('DB_NAME', 'sdl_db');    
+// define('DB_PASSWORD', "kevin123"); 
+define('DB_PASSWORD', "8db16bf5"); 
+
+// define('DB_NAME', 'sdl_db');   
+define('DB_NAME', 'heroku_e8eadf12fb086cd');    
+
 ?>
+
